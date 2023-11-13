5dd37215cc0e42af670814824e2bbdf95b1e646e
Dzarek <jarekjanas95@gmail.com>
2023-11-13 22:14:31 +0100

    PWA

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,11 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/logo192.png" />
+          <meta name="theme-color" content="#fff" />
+        </Head>
         <body>
           <Main />
           <NextScript />
