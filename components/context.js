3342b932af5b0ed5c7a0ea991863a43787cdc888
Dzarek <jarekjanas95@gmail.com>
2023-11-19 19:28:10 +0100

    notification sw

--- components/context.js
+++ components/context.js
@@ -272,43 +272,102 @@ const AppProvider = ({ children }) => {
     });
   };
 
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("sw.js")
+        .then(function (registration) {
+          console.log(
+            "Service Worker registered with scope:",
+            registration.scope
+          );
+        })
+        .catch(function (error) {
+          console.error("Service Worker registration failed:", error);
+        });
+    }
+    if ("Notification" in window) {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") {
+          console.log("Notification permission granted.");
+        }
+      });
+    }
+  }, []);
+
   // NOTIFICATION NEW TRANSFER
   const notification = (uniqueitemsArray) => {
-    Notification.requestPermission().then((perm) => {
-      if (isAdmin && uniqueitemsArray.length > 0) {
-        const newAddedTransfer = uniqueitemsArray.find((item) => {
-          return (
-            item.createdDate < Date.now() &&
-            item.createdDate > moment().subtract(10, "seconds").valueOf()
-          );
+    if (isAdmin && uniqueitemsArray.length > 0) {
+      const newAddedTransfer = uniqueitemsArray.find((item) => {
+        return (
+          item.createdDate < Date.now() &&
+          item.createdDate > moment().subtract(10, "seconds").valueOf()
+        );
+      });
+      if (
+        Notification.permission === "granted" &&
+        newAddedTransfer !== undefined &&
+        newAddedTransfer.status === "pending"
+      ) {
+        navigator.serviceWorker.ready.then(function (registration) {
+          registration.showNotification("Dodano nowy transfer", {
+            body: `DATA: ${newAddedTransfer.date}, GODZINA: ${newAddedTransfer.time}`,
+            icon: "logo192.png",
+            tag: newAddedTransfer.id,
+            vibrate: [200, 100, 200],
+          });
         });
-        // console.log(newAddedTransfer);
-        if (
-          perm === "granted" &&
-          newAddedTransfer !== undefined &&
-          newAddedTransfer.status === "pending"
-        ) {
-          new Notification("Dodano nowy transfer", {
-            body: `DATA: ${newAddedTransfer.date}, GODZINA: ${newAddedTransfer.time}`,
-            icon: "logo192.png",
-            tag: newAddedTransfer.id,
-            vibrate: [200, 100, 200],
-          });
-        }
-        if (
-          perm === "granted" &&
-          newAddedTransfer !== undefined &&
-          newAddedTransfer.status === "cancel"
-        ) {
-          new Notification("Anulowano transfer", {
-            body: `DATA: ${newAddedTransfer.date}, GODZINA: ${newAddedTransfer.time}`,
-            icon: "logo192.png",
-            tag: newAddedTransfer.id,
-            vibrate: [200, 100, 200],
-          });
-        }
       }
-    });
+      if (
+        Notification.permission === "granted" &&
+        newAddedTransfer !== undefined &&
+        newAddedTransfer.status === "cancel"
+      ) {
+        navigator.serviceWorker.ready.then(function (registration) {
+          registration.showNotification("Anulowano transfer", {
+            body: `DATA: ${newAddedTransfer.date}, GODZINA: ${newAddedTransfer.time}`,
+            icon: "logo192.png",
+            tag: newAddedTransfer.id,
+            vibrate: [200, 100, 200],
+          });
+        });
+      }
+    }
+    // Notification.requestPermission().then((perm) => {
+    //   if (isAdmin && uniqueitemsArray.length > 0) {
+    //     const newAddedTransfer = uniqueitemsArray.find((item) => {
+    //       return (
+    //         item.createdDate < Date.now() &&
+    //         item.createdDate > moment().subtract(10, "seconds").valueOf()
+    //       );
+    //     });
+    //     // console.log(newAddedTransfer);
+    //     if (
+    //       perm === "granted" &&
+    //       newAddedTransfer !== undefined &&
+    //       newAddedTransfer.status === "pending"
+    //     ) {
+    //       new Notification("Dodano nowy transfer", {
+    //         body: `DATA: ${newAddedTransfer.date}, GODZINA: ${newAddedTransfer.time}`,
+    //         icon: "logo192.png",
+    //         tag: newAddedTransfer.id,
+    //         vibrate: [200, 100, 200],
+    //       });
+    //     }
+    //     if (
+    //       perm === "granted" &&
+    //       newAddedTransfer !== undefined &&
+    //       newAddedTransfer.status === "cancel"
+    //     ) {
+    //       new Notification("Anulowano transfer", {
+    //         body: `DATA: ${newAddedTransfer.date}, GODZINA: ${newAddedTransfer.time}`,
+    //         icon: "logo192.png",
+    //         tag: newAddedTransfer.id,
+    //         vibrate: [200, 100, 200],
+    //       });
+    //     }
+    //   }
+    // });
     // END NOTIFICATION NEW TRANSFER
   };
 
